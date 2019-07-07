d589e887b562c242b47727d6b861bec787f9e559
Jorge Arboleda <jorgearbolex@gmail.com>
2019-07-06 22:03:28 -0500

    FPS
    Mi primer fisrst person shutter

---
+++ Assets/FPSAmi.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSAmi : MonoBehaviour
+{
+    public float mouseX; // variable establecida fuera del update para eje X
+    public float mouseY; // variable establecida fuera del update para eje Y
+    public bool InvertedMouse; // boleano para invertir la posición la cámara
+    public float speed = 1; // float para multiplicar la velocidad de movimiento con mouse Y la función Vector3
+
+
+
+    void Update()
+    {
+        mouseX += Input.GetAxis("Mouse X");
+
+        if (InvertedMouse) // opción para movimiento de camara más armonico y menos tembloroso 
+        {
+            mouseY += Input.GetAxis("Mouse Y"); //incremento en eje X con Get.Axis
+        }
+        else
+        {
+            mouseY -= Input.GetAxis("Mouse Y"); //sino, decremento en eje Y con Get.Axis
+        }
+        Debug.Log(mouseX);
+        transform.eulerAngles = new Vector3(mouseY*speed, mouseX*speed,0); 
+        //transform para la rotación en los ejes X y Y de la cámara con eulerAngels y multiplicar la velocidad por cada uno de ellos
+    }
+
+
+
+
+
+
+}
---
+++ Assets/FPSMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSMove : MonoBehaviour // esta clase contiene los movimientos del objeto y a su vez la clase de la camara dentro del objeto
+{
+    public float speed = 0.3f; // definimos las variable float que da la velocidad de desplazamiento
+    public FPSAmi rota; // llamamos la clase FPSAmi que contiene el la orientación y movimiento de la camara
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)) //creamos las funciones para cada una de las cuatro teclas que permiten el dezplazamiento del objeto esta es W hacia adelante
+        {
+            transform.position += transform.forward * speed;
+        }
+        if (Input.GetKey(KeyCode.S)) // esta es S para dezplazamiento hacia atras donde ponemos en negativo el movimiento de forward
+        {
+            transform.position -= transform.forward * speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * speed;
+        }
+        if (Input.GetKey(KeyCode.D)) // esta es D para dezplazamiento hacia la izquierda, donde ponemos en negativo el movimiento de right
+        {
+            transform.position += transform.right * speed;
+
+        }
+        transform.eulerAngles = new Vector3(0, rota.mouseX*speed,0); //esta es la función que permite girar la camara en el eje Y, y también permite que para donde se mire se dezplase el objeto
+    }
+
+}
